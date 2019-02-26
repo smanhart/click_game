5b7ea65f0ebe211b3b6680d0191de9e9d819f93d
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-02-25 21:52:18 -0700

    Updates

---
+++ precache-manifest.596ccdfffa3af86a9e43cbaa4b1c67d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f26da628c7f66b4aae097e1962b9dd1",
+    "url": "/click_game/static/media/pink_rice.5f26da62.png"
+  },
+  {
+    "revision": "5f1f0d283fb7f7381294",
+    "url": "/click_game/static/js/runtime~main.5f1f0d28.js"
+  },
+  {
+    "revision": "3f0f2f39b15185a53a92",
+    "url": "/click_game/static/js/main.3f0f2f39.chunk.js"
+  },
+  {
+    "revision": "cdb0db77f8e3e843b89f",
+    "url": "/click_game/static/js/2.cdb0db77.chunk.js"
+  },
+  {
+    "revision": "3f0f2f39b15185a53a92",
+    "url": "/click_game/static/css/main.f92f127f.chunk.css"
+  },
+  {
+    "revision": "43d197ecf183032cae7a9e49977b52ca",
+    "url": "/click_game/index.html"
+  }
+];
